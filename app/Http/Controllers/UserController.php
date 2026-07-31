bf5f43462333ada8dcf4c7df3cb5072bf48e367f
yian-zh <yzhou@paragoniu.edu.kh>
2026-07-31 20:01:14 +0700

    fix(users): import DB facade in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
